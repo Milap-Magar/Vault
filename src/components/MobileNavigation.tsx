22342279f61200c37fa35fd5990331d3e375dd97
milap-magar <milapmagar12@gmail.com>
2024-11-17 14:29:29 +0545

    Added: Logout functionality added

--- src/components/MobileNavigation.tsx
+++ src/components/MobileNavigation.tsx
@@ -17,6 +17,7 @@ import Link from "next/link";
 import { cn } from "@/lib/utils";
 import { Button } from "./ui/button";
 import FileUploader from "./FileUploader";
+import { signOutUser } from "@/lib/actions/user.actions";
 
 interface Props {
   ownerId: string;
@@ -98,7 +99,7 @@ const MobileNavigation = ({
             <Button
               type="submit"
               className="mobile-sign-out-button"
-              onClick={() => {}}
+              onClick={async () => await signOutUser()}
             >
               <Image
                 src="/icon/logout.svg"
