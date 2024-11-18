3aa08f24b02ab609135142f6a140282687ca3422
milap-magar <milapmagar12@gmail.com>
2024-11-18 20:23:41 +0545

    Dynamic Routing

---
+++ src/app/(root)/[type]/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const page = async ({ params }: SearchParamProps) => {
+  const type = ((await params)?.type as string) || "";
+  return <div className="page-container">
+    <section className="w-full ">
+        <h1 className="h1 capitalize"></h1>
+    </section>
+  </div>;
+};
+
+export default page;
